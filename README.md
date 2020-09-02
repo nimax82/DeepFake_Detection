680c1b04d170a6f2c18ba2e521df3cf4056dbffe
test <test@macbook-air-de-valentin.home>
2020-09-02 15:53:22 +0200

    final commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DNGI_Detection
--- handmade_video/prnu/Filter/mrdwt_r.h
+++
@@ -1,5 +0,0 @@
-#include <math.h>
-#include <stdio.h>
-
-void MRDWT(double *x, int m, int n, double *h, int lh, int L, double *yl, double *yh);
-void fpconv(double *x_in, int lx, double *h0, double *h1, int lh, double *x_outl, double *x_outh);
